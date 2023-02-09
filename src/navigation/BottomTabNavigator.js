dad4cf63607d4793e007ad1178e5b17e1d117821
nico martinez <divnicokovacdiv@gmail.com>
2023-02-09 15:53:44 -0300

    Desafio tags Clase 11

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -1,5 +1,7 @@
+import { StyleSheet, View, Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import Ionicons from "@expo/vector-icons/Ionicons";
+ 
 import CartNavigator from "./CartNavigator";
 import ShopNavigator from "./ShopNavigator";
 
@@ -9,11 +11,68 @@ const BottomTabs = createBottomTabNavigator ();
 
 export default BottomTabNavigator = () => {
     return(
-        <BottomTabs.Navigator>
+        <BottomTabs.Navigator 
+            initialRouteName="ShopTab" 
+            screenOptions={{
+            headerShown:false,
+            tabBarShowLabel:false,
+            tabBarStyle:styles.tabBar,
 
-            <BottomTabs.Screen name="ShopTab" component={ShopNavigator} />
-            <BottomTabs.Screen name="CartTab" component={CartNavigator}/>
+        }}>
+
+            <BottomTabs.Screen 
+                name="ShopTab" 
+                component={ShopNavigator}
+                options={{
+                    tabBarIcon: () => (
+                        <View style={styles.icon}>
+                            <Ionicons name="home" size={20} color="black" />
+                            <Text> Inicio </Text>
+                        </View>
+                    ),
+                }}
+            
+            />
+            <BottomTabs.Screen
+                 name="CartTab" 
+                 component={CartNavigator}
+                 options={{
+                    tabBarIcon: () => (
+                        <View style={styles.icon}>
+                            <Ionicons name="cart" size={20} color="black" />
+                            <Text> Carrito </Text>
+                        </View>
+                    )
+                 }}
+            />
 
         </BottomTabs.Navigator>
     )
 }
+
+const styles = StyleSheet.create({
+    tabBar: {
+        shadowColor: "#cc99ff",
+        shadowOffset: { width: 0, height: 0},
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        elevation: 5,
+        position: "absolute",
+        bottom: 10,
+        left:20,
+        right:20,
+        borderRadius:15,
+        height:60,
+
+    },
+
+    icon:{
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+})
+
+// headerShown en false para sacar la cabecera y titula del stack
+// el tabbar por ahora en true, luego en flase para poner iconos
+// tabBarStyle, estilos.
