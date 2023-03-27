2fdd8f2ab306d30d56b67e461ec2aa88905248b9
nico martinez <divnicokovacdiv@gmail.com>
2023-03-26 23:46:47 -0300

    App Coder

--- src/navigation/MapNavigator.js
+++ src/navigation/MapNavigator.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import MapView from 'react-native-maps';
+import React, {useState, useEffect} from 'react';
+import MapView, {Marker} from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import  * as Location from 'expo-location';
 
 export default function App() {
+
+    const [mapRegion, setMapRegion] = useState ({
+        //-33.43700644245535, -70.63431606556514
+        latitude: -33.43700644245535,
+        longitude:-70.63431606556514 ,
+        latitudeDelta: 0.5221,
+        longitudeDelta: 0.3525,
+
+    });
+
+    const userLocation = async () => {
+        let {status} = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'ganted'){
+            setErrorMsg('Permision Denied');
+            return;
+        }
+    }
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map}
+            region={mapRegion}
+      />
+      <Marker coordinate={mapRegion} title='Marker'/>
     </View>
   );
 }
